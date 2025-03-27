39a466a35085d4e281b63ebfb7a0b8a3745d0ac7
Giho <90423010+Levytein@users.noreply.github.com>
2025-03-26 22:41:25 -0400

    Update pool.ts

--- backend/db/pool.ts
+++ backend/db/pool.ts
@@ -4,12 +4,17 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const pool = new Pool({
+/*const pool = new Pool({
   host: process.env.HOST, 
   user: process.env.USER,
   database: process.env.DATABASE,
   password: process.env.DATA,
   port: process.env.PORT ? parseInt(process.env.PORT, 10) : undefined 
+});*/
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
-
 export default pool;
